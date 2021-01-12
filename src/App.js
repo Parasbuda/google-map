4aa4a2388412429da29f92e9baae454d0291f355
Parasbuda <parasbuda1234@gmail.com>
2021-01-12 15:03:20 +0545

    google map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+import { withGoogleMap, GoogleMap } from "react-google-maps";
+const App = () => {
+  const Example = withGoogleMap((props) => (
+    <GoogleMap
+      defaultCenter={{ lat: 27.6986, lng: 85.3356 }}
+      defaultZoom={13}
+    ></GoogleMap>
+  ));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Example
+        containerElement={<div style={{ height: `500px`, width: `500px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
     </div>
   );
-}
+};
 
 export default App;
